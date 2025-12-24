3681b1d38a44496bdceb0fd5f91a1b461b004d63
Boris <belyj3866@gmail.com>
2025-12-24 23:57:37 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,38 @@
+const cards = document.querySelector(".destinations__cards");
+const btnLeft = document.getElementById("slideLeft");
+const btnRight = document.getElementById("slideRight");
+
+const cardWidth = 300 + 32;
+let offset = 0;
+
+// ДУБЛИРУЕМ КАРТОЧКИ
+cards.innerHTML += cards.innerHTML;
+
+btnRight.addEventListener("click", () => {
+  offset -= cardWidth;
+  cards.style.transition = "transform 0.4s ease";
+  cards.style.transform = `translateX(${offset}px)`;
+
+  // если дошли до середины дубликата — сброс без анимации
+  if (Math.abs(offset) >= cards.scrollWidth / 2) {
+    setTimeout(() => {
+      cards.style.transition = "none";
+      offset = 0;
+      cards.style.transform = `translateX(${offset}px)`;
+    }, 400);
+  }
+});
+
+btnLeft.addEventListener("click", () => {
+  offset += cardWidth;
+  cards.style.transition = "transform 0.4s ease";
+  cards.style.transform = `translateX(${offset}px)`;
+
+  if (offset > 0) {
+    setTimeout(() => {
+      cards.style.transition = "none";
+      offset = -cards.scrollWidth / 2;
+      cards.style.transform = `translateX(${offset}px)`;
+    }, 400);
+  }
+});
